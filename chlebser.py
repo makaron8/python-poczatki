2c735ef47c4c42d00edf57fe8f76c1f2a432131b
makaron8 <97914259+makaron8@users.noreply.github.com>
2022-03-06 01:06:48 +0100

    Create chlebser.py

---
+++ chlebser.py
@@ -0,0 +1,11 @@
+chleb = 7
+ser = 7
+
+if chleb > ser:
+    print( chleb , 'jest większy od' , ser)
+
+elif chleb < ser:
+    print( ser , 'jest więkswzy od' , chleb)    
+
+elif chleb == ser:
+    print(ser , 'jest równy' , chleb)
